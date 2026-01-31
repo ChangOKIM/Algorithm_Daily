31271adb658467b9d1da7d495001e5d760ee1090
ChangOKIM <103881270+ChangOKIM@users.noreply.github.com>
2026-01-31 19:35:25 +0900

    Title: 남녀노소 구분짓기, Time: 87ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 260131/남녀노소 구분짓기/sex-and-age.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sex = Integer.parseInt(br.readLine());
+
+        int age = Integer.parseInt(br.readLine());
+
+        if(sex == 0){
+            if(age>19){
+                System.out.println("MAN");
+            }else
+                System.out.println("BOY");
+        }else{
+            if(age>19){
+                System.out.println("WOMAN");
+            }else{
+                System.out.println("GIRL");
+            }
+        }
+
+    }
+}
