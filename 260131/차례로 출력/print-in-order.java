1075242685a00143e1677610a2a0487e50074e9b
ChangOKIM <103881270+ChangOKIM@users.noreply.github.com>
2026-01-31 18:44:24 +0900

    Title: 차례로 출력, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260131/차례로 출력/print-in-order.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // Please write your code here.
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i =5; i<18; i++){
+            sb.append(i).append(" ");
+        }
+
+        System.out.println(sb);
+
+    }
+}
