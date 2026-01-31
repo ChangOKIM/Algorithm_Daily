601c1708a4d7f04e702066c347e318948811189f
ChangOKIM <103881270+ChangOKIM@users.noreply.github.com>
2026-01-31 19:59:45 +0900

    Title: 배열의 합 계산하기, Time: 91ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260131/배열의 합 계산하기/calculate-sum-of-array.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int[] A = {3, 1, 4, 5, 9};
+
+        System.out.println(A[1] + A[3] + A[4]);
+    }
+}
