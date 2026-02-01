9babd8502d0d69f48340b3a34627ac750cc1eb9a
ChangOKIM <103881270+ChangOKIM@users.noreply.github.com>
2026-02-01 15:02:40 +0900

    Title: 특정 원소들의 합, Time: 84ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260201/특정 원소들의 합/sum-of-specific-elements.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[][] arr = new int[4][4];
+        
+
+        for(int i=0; i<4; i++){
+            String str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str);
+            for(int j=0; j<4; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int sum = 0;
+        for(int i=0; i<4; i++){
+            for(int j=0; j<i+1; j++){
+                sum += arr[i][j];
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
