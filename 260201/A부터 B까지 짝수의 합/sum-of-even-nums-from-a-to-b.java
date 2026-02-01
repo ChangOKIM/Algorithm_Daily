cfb910c43c845bfd0e5b4f0e6c9909a666d76354
ChangOKIM <103881270+ChangOKIM@users.noreply.github.com>
2026-02-01 11:52:52 +0900

    Title: A부터 B까지 짝수의 합, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260201/A부터 B까지 짝수의 합/sum-of-even-nums-from-a-to-b.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(str);
+
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int sum = 0;
+
+        for(int i=A; i<B+1; i++){
+            if(i%2==0){
+                sum += i;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
