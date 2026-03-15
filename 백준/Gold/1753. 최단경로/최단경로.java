5762a84b25f6a8369804a9e0ec1330cd0e97bb29
ChangOKIM <103881270+ChangOKIM@users.noreply.github.com>
2026-03-15 23:33:36 +0900

    [Gold IV] Title: 최단경로, Time: 656 ms, Memory: 108284 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,85 +2,85 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int V, E, K;
-    static ArrayList<Node>[] graph;
-    static int[] distance;
-    static StringTokenizer st;
-    static final int INF = Integer.MAX_VALUE;
-
-    static class Node implements Comparable<Node> {
-        int end;
-        int weight;
-
-        public Node(int end, int weight) {
-            this.end = end;
-            this.weight = weight;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return this.weight - o.weight;
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        st = new StringTokenizer(br.readLine());
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-
-        K = Integer.parseInt(br.readLine());
-
-        graph = new ArrayList[V + 1];
-        distance = new int[V + 1];
-
-        for (int i = 1; i <= V; i++) {
-            graph[i] = new ArrayList<>();
-            distance[i] = INF;
-        }
-
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-
-            graph[u].add(new Node(v, w));
-        }
-
-        dijkstra(K);
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= V; i++) {
-            if (distance[i] == INF) sb.append("INF").append("\n");
-            else sb.append(distance[i]).append("\n");
-        }
-
-        System.out.print(sb);
-    }
-
-    static void dijkstra(int start) {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(start, 0));
-        distance[start] = 0;
-
-        while (!pq.isEmpty()) {
-            Node now = pq.poll();
-
-            int nowEnd = now.end;
-            int nowWeight = now.weight;
-
-            if (distance[nowEnd] < nowWeight) continue;
-
-            for (int i = 0; i < graph[nowEnd].size(); i++) {
-                Node next = graph[nowEnd].get(i);
-
-                if (distance[next.end] > nowWeight + next.weight) {
-                    distance[next.end] = nowWeight + next.weight;
-                    pq.add(new Node(next.end, distance[next.end]));
-                }
-            }
-        }
-    }
+	
+	static class Node implements Comparable<Node>{
+		int to, cost;
+		
+		Node(int to, int cost){
+			this.to = to;
+			this.cost = cost;
+		}
+		
+		public int compareTo(Node o) {
+			return this.cost - o.cost;
+		}
+	}
+	
+	static StringTokenizer st;
+	static int V, E, K;
+	static ArrayList<Node>[] graph;
+	static final int INF = Integer.MAX_VALUE;
+	static int[] dist;
+	
+	
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		st = new StringTokenizer(br.readLine());
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		
+		K = Integer.parseInt(br.readLine());
+		
+		graph = new ArrayList[V + 1];
+		dist = new int[V+1];
+		
+		for(int i=1; i<= V; i++) {
+			graph[i] = new ArrayList<>();
+			dist[i] = INF;
+		}
+		
+		for(int i=0; i<E; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			
+			graph[u].add(new Node(v, w));
+		}
+		
+		dijkstra(K);
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i=1; i<=V; i++) {
+			if(dist[i] == INF) sb.append("INF").append("\n");
+			else sb.append(dist[i]).append("\n");
+		}
+		
+		System.out.println(sb);
+		
+		
+	}
+	
+	static void dijkstra(int start) {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.add(new Node(start, 0));
+		dist[start] = 0;
+		
+		while(!pq.isEmpty()) {
+			Node now = pq.poll();
+			
+			if(dist[now.to] < now.cost) continue;
+			
+			for(Node next : graph[now.to]) {
+				int nextCost = dist[now.to] + next.cost;
+				
+				if(dist[next.to] > nextCost) {
+					dist[next.to] = nextCost;
+					pq.add(new Node(next.to, nextCost));
+				}
+			}
+		}
+	}
 }
