b1676aae4b405042434d824f2424b7a462135a0b
ChangOKIM <103881270+ChangOKIM@users.noreply.github.com>
2026-03-30 16:11:24 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14520 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,63 +1,63 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
 	static int N, M;
 	static int[][] graph;
 	static boolean[][] visited;
 	static StringTokenizer st;
-	static int[] dx = {1, 0, -1, 0};
+	static int[] dx =  {1, 0, -1, 0};
 	static int[] dy = {0, 1, 0, -1};
-	static int max_val;
+	static ArrayDeque<int[]> queue;
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		st = new StringTokenizer(br.readLine());
-		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
 		graph = new int[N][M];
 		visited = new boolean[N][M];
 		
+		
 		for(int i=0; i<N; i++) {
-			String str = br.readLine();
-			for(int j=0; j<M; j++) {
-				graph[i][j] = str.charAt(j) - '0';
+			String s = br.readLine();
+			for(int j=0; j<s.length(); j++) {
+				graph[i][j] = s.charAt(j) - '0';
 			}
 		}
 		
 		bfs(0, 0);
-
+		
 		System.out.println(graph[N-1][M-1]);
 		
 	}
 	
 	static void bfs(int x, int y) {
-		Deque<int[]> queue = new ArrayDeque<>();
-		
+		queue = new ArrayDeque<>();
 		queue.add(new int[] {x, y});
 		visited[x][y] = true;
 		
 		while(!queue.isEmpty()) {
-			int[] check = queue.pollFirst();
-			int cx = check[0];
-			int cy = check[1];
+			int[] current = queue.pollFirst();
+			
+			int cx = current[0];
+			int cy = current[1];
 			
 			for(int i=0; i<4; i++) {
 				int nx = cx + dx[i];
 				int ny = cy + dy[i];
 				
-				if(0<= nx && nx<N && 0<=ny && ny<M) {
-					if(!visited[nx][ny] && graph[nx][ny] >= 1) {
+				if(0<= nx && nx < N && 0<= ny && ny < M) {
+					if(graph[nx][ny] > 0 && !visited[nx][ny]) {
+						queue.add(new int[] {nx, ny});
 						graph[nx][ny] = graph[cx][cy] + 1;
 						visited[nx][ny] = true;
-						queue.add(new int[] {nx, ny});
 					}
 				}
 			}
 		}
 	}
 }
-
