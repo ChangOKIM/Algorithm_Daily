9570a221b581f2f6807b6860cb55cd5580608b4c
ChangOKIM <103881270+ChangOKIM@users.noreply.github.com>
2026-04-03 22:37:41 +0900

    [Silver III] Title: N과 M (3), Time: 384 ms, Memory: 67960 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -2,46 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N;
-	static int M;
+	static int N, M;
 	static int[] arr;
 	static int[] temp;
-	static StringBuilder sb = new StringBuilder();
-
-	public static void main(String[] args) throws Exception {
-
+	static StringTokenizer st;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		N = Integer.parseInt(st.nextToken());
-		arr = new int[N];
-
-		for(int i=1; i<=N; i++){
-			arr[i-1] = i;
-		}
-		M = Integer.parseInt(st.nextToken());
-		temp = new int[M];
-
-		combination(0);
-
-		System.out.println(sb);
 		
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[N];
+		temp = new int[M];
+		
+		for(int i=0; i<N; i++) {
+			arr[i] = i+1;
+		}
+		
+		sb = new StringBuilder();
+		
+		dfs(0);
+		
+		System.out.println(sb);
 	}
-
-	static void combination(int depth){
-		if(depth == M){
-			for(int i=0; i<M; i++){
+	
+	static void dfs(int depth) {
+		if(depth == M) {
+			for(int i=0; i<M; i++) {
 				sb.append(temp[i]).append(" ");
 			}
 			sb.append('\n');
 			return;
 		}
-
-		for(int i=0; i<N; i++){
+		
+		
+		for(int i=0; i<N; i++) {
 			temp[depth] = arr[i];
-			combination(depth+1);
-
+			dfs(depth + 1);
 		}
+		
+		
 	}
 }
