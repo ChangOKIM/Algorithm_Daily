b6589126bf2a56899f4e56b495d265cb7bdb1a5e
ChangOKIM <103881270+ChangOKIM@users.noreply.github.com>
2026-04-04 15:27:17 +0900

    [Silver II] Title: 로또, Time: 116 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -2,37 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int k, check;
+	static int N;
 	static int[] arr;
-	static boolean[] visited;
-	static StringTokenizer st;
 	static int[] temp;
-	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	static StringBuilder sb;
+	static ArrayList<Integer> init;
 	
 	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		k = 18;
+		sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		while(true) {
-			st = new StringTokenizer(br.readLine());
-			k = Integer.parseInt(st.nextToken());
-			if(k == 0) break;
-			arr = new int[k];
-			for(int i=0; i<k; i++) {
-				arr[i] = Integer.parseInt(st.nextToken());
-			}
-			
-			temp = new int[6];
-			visited = new boolean[k];
 
-			dfs(0, 0);
-			sb.append('\n');
+			st = new StringTokenizer(br.readLine());
+			
+			N = Integer.parseInt(st.nextToken());
+			
+			if(N == 0) {
+				System.out.println(sb);
+				break;
+			}else {
+				arr = new int[N];
+				temp = new int[6];
+				
+				for(int i=0; i<N; i++) {
+					arr[i] = Integer.parseInt(st.nextToken());
+				}
+								
+				dfs(0, 0);
+				sb.append('\n');
+			}
 		}
-		
-		System.out.println(sb);
 	}
 	
-	static void dfs(int depth, int num) {
+	static void dfs(int depth, int start) {
 		if(depth == 6) {
 			for(int i=0; i<6; i++) {
 				sb.append(temp[i]).append(" ");
@@ -41,15 +45,11 @@ public class Main {
 			return;
 		}
 		
-		for(int i=num; i<k; i++) {
-			if(visited[i]) continue;
-			
-			visited[i] = true;
+		for(int i=start; i<N; i++) {
 			temp[depth] = arr[i];
 			dfs(depth+1, i+1);
-			
-			visited[i] = false;
 		}
 	}
-	
+		
+		
 }
