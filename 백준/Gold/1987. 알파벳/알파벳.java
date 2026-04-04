f697fafabbdd32740cb079eeda36ba4fcc5b87cd
ChangOKIM <103881270+ChangOKIM@users.noreply.github.com>
2026-04-04 16:39:45 +0900

    [Gold IV] Title: 알파벳, Time: 3464 ms, Memory: 20076 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -2,70 +2,70 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int R, C;
-	static char[][] graph;
-	static boolean[][] visited;
-	static boolean[] alphabet;
-	static StringTokenizer st;
-	static int maxMove;
-	static int[] dx = {1, 0, -1, 0};
-	static int[] dy = {0, 1, 0, -1};
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		st = new StringTokenizer(br.readLine());
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		
-		graph = new char[R][C];
-		visited = new boolean[R][C];
-		alphabet = new boolean[26];
-		maxMove = 1;
-		
-		
-		for(int i=0; i<R; i++) {
-			String s = br.readLine();
-			for(int j=0; j<C; j++) {
-				graph[i][j] = s.charAt(j);
-			}
-		}
-		alphabet[graph[0][0] - 'A'] = true;
-		
-		dfs(0, 0, 1);
-		System.out.println(maxMove);
-	}
-	
-	static void dfs(int x, int y, int move) {		
-		if(move > maxMove) {
-			maxMove = move;
-		}
-		
-		for(int i=0; i<4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-			
-			if(0<= nx && nx<R && 0<= ny && ny<C) {
-				int idx = graph[nx][ny] - 'A';
-				
-				if(!visited[nx][ny] && !alphabet[idx]) {
-					visited[nx][ny] = true;
-					alphabet[idx] = true;
-					
-					dfs(nx, ny, move +1);
-					
-					visited[nx][ny] = false;
-					alphabet[idx] = false;
-				}
-			}
-		}
-	}
-		
+    static int R, C;
+    static char[][] graph;
+    static boolean[][] visited;
+    static ArrayList<Character> alphabet;
+    static StringTokenizer st;
+    static int maxMove;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        graph = new char[R][C];
+        visited = new boolean[R][C];
+        alphabet = new ArrayList<>();
+        maxMove = 1;
+
+        for(int i = 0; i < R; i++) {
+            String s = br.readLine();
+            for(int j = 0; j < C; j++) {
+                graph[i][j] = s.charAt(j);
+            }
+        }
+
+        alphabet.add(graph[0][0]);
+        visited[0][0] = true;
+
+        dfs(0, 0);
+        System.out.println(maxMove);
+    }
+
+    static void dfs(int x, int y) {
+        if(alphabet.size() > maxMove) {
+            maxMove = alphabet.size();
+        }
+
+        for(int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(0 <= nx && nx < R && 0 <= ny && ny < C) {
+                if(!visited[nx][ny] && !usedAlpha(graph[nx][ny])) {
+                    visited[nx][ny] = true;
+                    alphabet.add(graph[nx][ny]);
+
+                    dfs(nx, ny);
+
+                    alphabet.remove(alphabet.size() - 1);
+                    visited[nx][ny] = false;
+                }
+            }
+        }
+    }
+
+    static boolean usedAlpha(char c) {
+        for(int i = 0; i < alphabet.size(); i++) {
+            if(alphabet.get(i) == c) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
-
-
-
-
-
-
-
