296299fa82500c936ffbf0d79ee471e33b87d8b1
ChangOKIM <103881270+ChangOKIM@users.noreply.github.com>
2026-04-04 11:57:55 +0900

    [Silver III] Title: N과 M (4), Time: 144 ms, Memory: 17348 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -2,45 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int arr[];
-    static int temp[];
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
+	static int N, M;
+	static int[] temp;
+	static StringTokenizer st; 
+	static StringBuilder sb;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
 
-
-    public static void main(String[] args)throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr =  new int[N];
-        temp = new int[M];
-
-        for(int i=1; i<N+1; i++){
-            arr[i-1] =  i;
-        }
-
-        
-
-        dfs(0, 0);
-        System.out.println(sb);
-    }
-
-    static void dfs(int depth, int num){
-        if(depth == M){
-            for(int i=0; i<M; i++){
-                sb.append(temp[i]).append(" ");
-            }
-            sb.append('\n');
-            return;
-        }
-
-        for(int i=num; i<N; i++){
-            temp[depth] = arr[i];
-
-            dfs(depth+1, i);
-
-        }
-    }
+		temp = new int[M];
+		sb = new StringBuilder();
+		
+		dfs(0, 1);
+		System.out.println(sb);
+	}
+	
+	static void dfs(int depth, int start) {
+		if(depth == M) {
+			for(int i=0; i<M; i++) {
+				sb.append(temp[i]).append(" ");
+			}
+			sb.append('\n');
+			return;
+		}
+		
+		for(int i=start; i<=N; i++) {
+			temp[depth] = i;
+			dfs(depth+1, i);
+			
+		}
+		
+	}
 }
+
+
+
+
+
+
+
