e54a3fe72c3e8eee12a17bb48f4261663f6a9730
ChangOKIM <103881270+ChangOKIM@users.noreply.github.com>
2026-04-08 10:03:00 +0900

    [Silver V] Title: 다리 놓기, Time: 140 ms, Memory: 15096 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,34 +1,38 @@
 import java.io.*;
-import java.math.BigInteger;
 import java.util.*;
 
+/*
+ * 메모리: 14,244kb, 실행시간: 104ms
+ */
+
 public class Main {
+	static int T;
 	static int N, M;
-	static BigInteger top, bottom;
+	static int[][] dp;
 	static StringTokenizer st;
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int tc = 1; tc<=T; tc++) {
+		T = Integer.parseInt(br.readLine());
+		for(int tc=1; tc<=T; tc++) {
 			st = new StringTokenizer(br.readLine());
-			
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
-			top = new BigInteger("1"); //문자열로 해야됨?
-			bottom = new BigInteger("1");
 			
-			for( int i=M; i>M-N; i--) {
-				top = top.multiply(BigInteger.valueOf(i));
+			dp = new int[M+1][N+1];
+			
+			for(int i=0; i<M; i++) {
+				dp[i][0] = 1;
 			}
 			
-			for(int i=1; i<=N; i++) {
-				bottom = bottom.multiply(BigInteger.valueOf(i));
+			for(int m=1; m<=M; m++) {
+				for(int n=1;n<=m && n<=N; n++) {
+					dp[m][n] = dp[m-1][n-1] + dp[m-1][n];
+				}
 			}
 			
-			System.out.println(top.divide(bottom));
+			System.out.println(dp[M][N]);
 		}
+	
 	}
 }
