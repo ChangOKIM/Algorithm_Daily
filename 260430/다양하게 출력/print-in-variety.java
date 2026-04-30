fcd8445055dc64081904d95a448d829bd2ee122d
ChangOKIM <103881270+ChangOKIM@users.noreply.github.com>
2026-04-30 11:02:46 +0900

    Title: 다양하게 출력, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

---
+++ 260430/다양하게 출력/print-in-variety.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Welcome to LeebrosCode!
+        Welcome to LeebrosCode!
+    }
+}
