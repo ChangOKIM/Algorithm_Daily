8e44779dd1ca481579fda3e88a51f1f4c13236fb
ChangOKIM <103881270+ChangOKIM@users.noreply.github.com>
2026-04-30 11:03:23 +0900

    Title: 다양하게 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260430/다양하게 출력/print-in-variety.java
+++ 260430/다양하게 출력/print-in-variety.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        Welcome to LeebrosCode!
-        Welcome to LeebrosCode!
+        System.out.println("Total days in Year");
+        System.out.println("365");
+        System.out.println("Circumference rate");
+        System.out.println("3.1415926535");
     }
 }
+
+
+
+
